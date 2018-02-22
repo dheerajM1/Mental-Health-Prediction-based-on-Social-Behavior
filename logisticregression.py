b4d883f8cfaca107d1dc8a0929a4bbd2e949aa93
dheerajM1 <dheerajmanchu@gmail.com>
2018-02-22 14:43:04 -0500

    Adding project files

---
+++ logisticregression.py
@@ -0,0 +1,71 @@
+from sklearn.linear_model import LogisticRegression
+
+def vocablist():
+
+    stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+
+    tweets = []
+    X = []
+    y = []
+    for line in open('tweets.txt').readlines():
+        items = line.split(',')
+        tweets.append([int(items[0]), items[1].lower().strip()])
+
+    # Extract the vocabulary of keywords
+    vocab = dict()
+    for class_label, text in tweets:
+        for term in text.split():
+            term = term.lower()
+            if len(term) > 2 and term not in stopwords:
+                if vocab.has_key(term):
+                    vocab[term] = vocab[term] + 1
+                else:
+                    vocab[term] = 1
+
+    vocab = {term: freq for term, freq in vocab.items() if freq > 10} # Remove terms whose frequencies are less than a threshold (e.g., 10)
+    sorted_vocab=sorted(vocab.items(), key=lambda x: x[1], reverse=True)
+    sorted10_vocab=sorted_vocab[0:30] # picked top 10 sorted keys
+    vocab1=dict(sorted10_vocab)
+    vocab = {term: idx for idx, (term, freq) in enumerate(vocab1.items())}  #Generate an id (starting from 0) for each term in vocab
+
+    for class_label, text in tweets:
+        x = [0] * len(vocab)
+        terms = [term for term in text.split() if len(term) > 2]
+        for term in terms:
+            if vocab.has_key(term):
+                x[vocab[term]] += 1
+        y.append(class_label)
+        X.append(x)
+    lr = LogisticRegression()
+    lr.fit(X, y)
+
+
+
+
+    # predict the class labels of new tweets
+
+    tweets = []
+    for line in open('unlabelled_tweets.txt').readlines():    #reading unlabelled tweet
+        tweets.append(line)
+
+    # Generate X for testing tweets
+    X = []
+    for text in tweets:
+        x = [0] * len(vocab)
+        terms = [term for term in text.split() if len(term) > 2]
+        for term in terms:
+            if vocab.has_key(term):
+                x[vocab[term]] += 1
+        X.append(x)
+    y = lr.predict(X)   #predicitng class label
+
+    hello = map(str, y) # toconvert into string
+    a = 0 #initialize the loop
+    file = open("predicted_tweetslr.txt", "a+")   #new file opened
+    for text in tweets:
+        labelled_tweet= hello[a]+","+text #appending predicted class and tweet text
+        file.write(labelled_tweet)   #writting the labelling tweet into file.
+        a += 1
+
+if __name__ == '__main__':
+    vocablist()
